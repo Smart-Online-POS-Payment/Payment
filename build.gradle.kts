70284e92236306019d0b721aef8faf6d5b4b2d0a
KaanSimsek19 <kaan.simsek@valensas.com>
2023-10-31 08:24:08 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+	id("org.jmailen.kotlinter") version "4.0.0"
+	kotlin("jvm") version "1.9.20-Beta2"
+	kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "com.sopp"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Spring
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	// Security
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+	//kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
